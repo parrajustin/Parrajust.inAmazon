be59c621fab0b07978bd92484eb41d7fb7a592eb
parrajustin <parra.justin.main@gmail.com>
2018-02-21 18:40:54 -0700

    Auto stash before merge of "master" and "origin/master"

--- server.js
+++ server.js
@@ -11,18 +11,42 @@ const methodOverride = require("method-override");
 const logger = require("morgan");
 const path = require("path");
 const express = require("express");
-const jsonfile = require('jsonfile');
+// const jsonfile = require('jsonfile');
+const basicAuth = require('express-basic-auth');
+const Engine = require('tingodb')();
+const _ = require('lodash');
+
 const app = express();
+const httpServer = http.createServer(app);
+const io = require('socket.io')(httpServer);
 
-var file = 'data.json'
-var codes = undefined;
-jsonfile.readFile(file, function(err, obj) {
-  if (err) {
-    throw new Error(err.message);
-  }
+const db = new Engine.Db('./data', {});
 
-  codes = obj;
-})
+// let queueRunning = false;
+// var file = 'data.json'
+// var codes = undefined;
+
+// jsonfile.readFile(file, function(err, obj) {
+//   if (err) {
+//     throw new Error(err.message);
+//   }
+
+//   codes = obj;
+
+//     const collection = db.collection('tokens');
+//     // Insert some documents
+//     collection.insert(_.map(obj, (o) => {
+//       return { token: o, forName: '', used: false, created: new Date(), createdBy: 'server-init', updated: new Date()};
+//     }), {}, function(err, result) {
+//       // assert.equal(err, null);
+//       // assert.equal(3, result.result.n);
+//       // assert.equal(3, result.ops.length);
+//       console.log("Inserted 3 documents into the collection");
+//       console.log(err);
+//       console.log(result);
+//     });
+// });
+
 
 //create http server
 var httpPort = normalizePort(8080);
@@ -31,7 +55,7 @@ var httpPort = normalizePort(8080);
 app.get('/access', (req, res) => {
   res.send('hello test');
 });
-//app.use('/', express.static(path.join(__dirname, 'public')));
+app.use('/', express.static(path.join(__dirname, 'public')));
 const allowCrossDomain = (req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,POST');
@@ -52,6 +76,86 @@ app.use((err, req, res, next) => {
   next(err);
 })
 app.use(errorHandler());
+app.get('/admin', basicAuth({
+  users: { 'justin': 'id384478', 'latrujano': 'Fackdis**10' },
+  challenge: true,
+  realm: 'MspCompanionChallenge',
+}), (req, res) => {
+  console.log(req.body);
+  console.log(req.auth);
+  res.status(200).sendFile(path.join(__dirname, 'public', 'token.html'));
+});
+app.post('/admin', basicAuth({
+  users: { 'justin': 'id384478', 'latrujano': 'Fackdis**10' },
+  challenge: true,
+  realm: 'MspCompanionChallenge',
+}), (req, res) => {
+  const date = new Date()
+  if (req.body['_id'] === '') {
+    db.collection("tokens").findOne({ token: req.body['token'] }, function(err, result) {
+      if (err) {
+        res.status(200).sendFile(path.join(__dirname, 'public', 'token.html'));
+        return;
+      }
+
+      if (result === null) {
+        db.collection('tokens').insert(
+          { 
+            token: req.body['token'], 
+            forName: req.body['name'], 
+            used: req.body['used'] == 'true', 
+            created: date, 
+            createdBy: req.auth['user'], 
+            updated: date
+          }
+        );
+        res.status(200).sendFile(path.join(__dirname, 'public', 'token.html'));
+      }
+    });
+  } else {
+    //       return { token: o, forName: '', used: false, created: new Date(), createdBy: 'server-init', updated: new Date()};
+    db.collection('tokens').findAndModify(
+      { _id: req.body['_id'] }, // query
+      [['_id', 'asc']],  // sort order
+      { $set: 
+        { 
+          token: req.body['token'], 
+          forName: req.body['name'],
+          used: req.body['used'] == 'true',
+          updated: date
+        } 
+      }, // replacement, replaces only the field "hi"
+      {}, // options
+      function (err, object) {
+        res.status(200).sendFile(path.join(__dirname, 'public', 'token.html'));
+      }
+    );
+  }
+
+});
+app.get('/tokens', basicAuth({
+  users: { 'justin': 'id384478', 'latrujano': 'Fackdis**10' },
+  challenge: true,
+  realm: 'MspCompanionChallenge',
+}), (req, res) => {
+  db.collection('tokens').find().toArray((err, docs) => {
+    res.status(200).json(docs);
+  });
+});
+io.on('connection', function (client) {
+  console.log('Client connected...');
+
+  client.on('join', function (data) {
+    console.log(data);
+  });
+
+  // client.on('messages', function (data) {
+  //   client.emit('broad', data);
+  //   client.broadcast.emit('broad', data);
+  // });
+
+});
+
 app.post('/access', (req, res) => {
   if (req.body.token === undefined || req.body.token !== '71c463c3d908fd07c14f784f622ea8b6') {
     res.status(300).json({ success: false, reason: 'All calls require an api key!' });
@@ -59,23 +163,26 @@ app.post('/access', (req, res) => {
   }
 
   let access = (req.body.code || '').trim();
-  let index = codes.indexOf(access);
-  if (access === '_MASTER_TOKEN_' || (codes !== undefined && index !== -1)) {
-    codes.splice(index, 1);
-
-    jsonfile.writeFile(file, codes, function (err) {
-      if (err) {
-        throw new Error(err.message);
+  let index;
+  if (access !== '_MASTER_TOKEN_') {
+    db.collection('tokens').findAndModify(
+      { token: access, used: false }, // query
+      [['_id', 'asc']],  // sort order
+      { $set: { used: true, usedDate: new Date() } }, // replacement, replaces only the field "hi"
+      {}, // options
+      function (err, object) {
+        if (err || object === undefined) {
+          res.status(300).json({ success: false, reason: 'either token has been used or doesn\'t exist' });
+        } else {
+          res.status(200).json({ success: true });
+        }
       }
-    });
-
-    res.status(200).json({ success: true });
+    );
   } else {
-    res.status(300).json({ success: false, reason: 'The provided access code was invalid' });
+    res.status(200).json({ success: true });
   }
 });
 
-const httpServer = http.createServer(app);
 
 //listen on provided ports
 httpServer.listen(httpPort);
